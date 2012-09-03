d24696c349035d6a605749400461fe8bff2896a9
Tuukka Turto <tuukka.turto@oktaeder.net>
2012-09-03 21:50:25 +0300

    licenses

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ breakfast
 =========
 
 Simple behave demo
+
+The breakfast code is copyrighted by Tuukka Turto and is placed under 
+GNU General Public License. For more information, please refer to license.txt
+found in the main folder.
--- features/steps/eggs.py
+++ features/steps/eggs.py
@@ -1,3 +1,20 @@
+#   Copyright 2012 Tuukka Turto
+#
+#   This file is part of breakfast, a behave demo.
+#
+#   breakfast is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   breakfast is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with breakfast.  If not, see <http://www.gnu.org/licenses/>.
+
 from breakfast import BreakfastMachine
 from ui import BreakfastUI
 from mockito import verify, mock
